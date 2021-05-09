c07020d5e25117d1b170be16c48f41822f90aed5
RandMihkel <randmihkel@gmail.com>
2021-05-10 00:23:01 +0300

    further developed the front page of the website

--- index.php
+++ index.php
@@ -16,21 +16,92 @@ include("connect.php")
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <!-- Custom css -->
-    <link href="styles\signin.scss" rel="stylesheet">
+    <link href="styles\custom.scss" rel="stylesheet">
 
     <title>ThoughBoard</title>
-
   </head>
 
-  <body class="bg-light">
-    <div class="container text-center">
-      <div class="row">
-        <div class="col">
-          <h2>Hello</h2>
-          <?php //include("login.php") ?>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">ThoughtBoard</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Team</a>
+            </li>          
+          </ul>
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item ">
+              <a class="nav-link" href="login.php">Log in</a>
+            </li>              
+          </ul>
         </div>
       </div>
-    </div>
+    </nav>
+
+    <main>   
+      <section class="py-5 text-center container-fluid mb-5 cover">
+        <div class="row py-lg-5">
+          <div class="col-lg-6 col-md-8 mx-auto">
+            <h1 class="fw-light">Organize your thoughts!</h1>
+            <p class="lead text-muted">A new and clunky way to keep track of all those pesky ideas of yours.</p>
+          </div>
+        </div>
+      </section>      
+
+      <div class="container">
+        <div class="row align-items-md-stretch">
+          <div class="col-md-6 mb-3">
+            <div class="h-100 p-5 text-white bg-dark rounded-3">
+              <h2>Change the background</h2>
+              <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+            </div>
+          </div>
+          <div class="col-md-6 mb-3">
+            <div class="h-100 p-5 bg-light border rounded-3">
+              <h2>Add borders</h2>
+              <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+            </div>
+          </div>
+          <div class="col-md-12 mb-3">
+            <div class="h-100 p-5 text-white bg-dark rounded-3">
+              <h2>Add borders</h2>
+              <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row g-2">
+          <div class="col-md-6">
+            <div class="p-4 border">Custom column padding</div>
+          </div>
+          <div class="col-md-6 ">
+            <div class="p-4 border">Custom column padding</div>
+          </div>
+          <div class="col-md-6">
+            <div class="p-4 border">Custom column padding</div>
+          </div>
+          <div class="col-md-6">
+            <div class="p-4 border">Custom column padding</div>
+          </div>
+          <div class="col-md-6">
+            <div class="p-4 border">Custom column padding</div>
+          </div>
+          <div class="col-md-6">
+            <div class="p-4 border">Custom column padding</div>
+        </div>
+      </div> 
+    </main>
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
   </body>
--- login.php
+++ login.php
@@ -1,23 +1,61 @@
-<main class="form-signin">
-        <form>
-          <img class="mb-4" src="img/logo.png" alt="logo image" width="100" height="100">
-          <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-        
-          <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
-        
-          <div class="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me"> Remember me
-            </label>
-          </div>
-          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-          <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
-        </form>
-    </main>  
+<?php 
+/*
+Mihkel Rand
+09.05.2021
+*/
+?>
+<!doctype html>
+<html lang="en">
+  <main class="form-signin">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-2 p-4 text-white bg-dark test-account">
+          <p>Use this account to log in for testing purposes without having to create a whole new account.
+          <br><br>
+          <b>Username: </b>test<br>
+          <b>Password: </b>1234
+          </p>
+        </div>        
+        <div class="col-md-5">
+          <form>
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>    
+            <div class="form-floating">
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+              <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+              <label for="floatingPassword">Password</label>
+            </div>
+          
+            <div class="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me"> Remember me
+              </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+          </form>
+        </div>
+        <div class="col-md-5">
+          <form>
+            <h1 class="h3 mb-3 fw-normal">or sign up!</h1>
+            <div class="form-floating">
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+              <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+              <label for="floatingPassword">Password</label>
+            </div>    
+            <div class="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me"> Remember me
+              </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+          </form>          
+        </div> 
+      </div>
+    </div>
+  </main>
+</html>  
