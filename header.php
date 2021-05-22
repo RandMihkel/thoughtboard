b56fc9d24f5a4ad290ab8fdb5804eb7cd74e8d0e
Mihkel Rand <randmihkel@gmail.com>
2021-05-23 00:50:27 +0300

    Removed the header section from index and created a header.php file

---
+++ header.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+Mihkel Rand
+22.05.2021
+*/
+?>
+
+<nav class="navbar navbar-expand-md navbar-light">
+    <div class="container">
+    <a class="navbar-brand" href="#">ThoughtBoard</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Team</a>
+        </li>          
+        </ul>
+        <ul class="navbar-nav ms-auto">
+        <li class="nav-item ">
+            <a class="nav-link" href="login.php">Log in</a>
+        </li>              
+        </ul>
+    </div>
+    </div>
+</nav>
